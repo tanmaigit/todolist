2439c5a3e5fef7598632bfebe35fda5db0da2aaf
tanmai <tanmai@kms-technology.com>
2018-01-07 13:28:21 +0700

    Write readme.md file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+		$userId = DB::table('user')->insertGetId([
+			'name' => 'Administrator',
+			'username' => 'admin',
+			'email' => 'admin@gmail.com',
+			'password' => Hash::make('admin')
+		]);
+		DB::table('todo')->insert([
+			'name' => 'first task',
+			'description' => 'reseacher the laravel/lumen and microservice, setup on local',
+			'status' => 3,
+			'priority' => 3,
+			'user_id' => $userId,
+			'created_at' => date('Y-m-d H:i:s', time())
+		]);
+		DB::table('todo')->insert([
+			'name' => 'second task',
+			'description' => 'create login page and create new user page',
+			'status' => 2,
+			'priority' => 1,
+			'user_id' => $userId,
+			'created_at' => date('Y-m-d H:i:s', time())
+		]);
+		DB::table('todo')->insert([
+			'name' => 'third task',
+			'description' => 'create todo list, create new todo, edit a todo',
+			'status' => 1,
+			'priority' => 2,
+			'user_id' => $userId,
+			'created_at' => date('Y-m-d H:i:s', time())
+		]);
+		echo 'Seed successfully.';
     }
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,29 @@
+# Introduction
+
+ToDo List Project is designed as Single Page Application with Microservice.
+Using Lumen 5.3 framework, AngularJS 1.5 and Twitter Bootstrap 3
+
+# Setup
+
+**cd** to **your_project** and run these commands:
+
+## Install dependencies
+```
+composer install
+bower install
+```
+
+## Create the database schema
+```
+php artisan migrate
+```
+
+## Insert some data for testing
+```
+php artisan db:seed
+```
+
+## Start server
+```
 php -S localhost:8000 -t public
+```
